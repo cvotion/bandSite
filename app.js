83e3666c74e7095e4624b2f2dbbbc12912cae418
Christian Votion <christianvotion@gmail.com>
2022-09-06 13:54:49 -0500

    heroku

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const app = express();
 const socket = require('socket.io');
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
